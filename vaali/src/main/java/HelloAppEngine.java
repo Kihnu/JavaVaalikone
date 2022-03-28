f8cd6cce443d970daf06d3ca75da7152cccf137c
JanetteLehto <75020018+JanetteLehto@users.noreply.github.com>
2022-03-28 10:40:08 +0300

    perkele

--- vaali/src/main/java/HelloAppEngine.java
+++ vaali/src/main/java/HelloAppEngine.java
@@ -23,7 +23,7 @@ public class HelloAppEngine extends HttpServlet {
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
 
-    response.getWriter().print("Hello Maailam!\r\n");
+    response.getWriter().print("Hello kddjöa!\r\n");
 
   }
 }
