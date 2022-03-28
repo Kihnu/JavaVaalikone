fdefadc8529afc3e9ac8e8e535b21636fdf7bc50
Sipuli69 <toni20113@student.hamk.fi>
2022-03-28 14:56:33 +0300

    En tie mitä oon tehny

--- vaali/src/main/java/app/HelloAppEngine.java
+++ vaali/src/main/java/app/HelloAppEngine.java
@@ -1,7 +1,6 @@
 package app;
 
 import java.io.IOException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +20,10 @@ public class HelloAppEngine extends HttpServlet {
 @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
 
+    response.getWriter().println("fuck off");
 
   }
 }
