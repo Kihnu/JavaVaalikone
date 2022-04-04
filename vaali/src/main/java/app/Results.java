37c4221bb710df080d3d0907c049290c56b0ca22
Erik Kihn <erikkihn97@gmail.com>
2022-04-04 15:09:11 +0300

    Update Results.java
    Sisennetty kunnolla koodi

--- vaali/src/main/java/app/Results.java
+++ vaali/src/main/java/app/Results.java
@@ -20,52 +20,53 @@ import data.Comparison;
 @WebServlet("/Results")
 public class Results extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+
 	private Dao dao;
-	
+
 	@Override
 	public void init() {
 		dao = new Dao("jdbc:mysql://localhost:3306/", "user", "password");
 	}
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Results() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Results() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//ArrayList<data.Comparison> comparison = null;
-		
-		
+		// ArrayList<data.Comparison> comparison = null;
+
 		ArrayList<Comparison> comparison = null;
 		ArrayList<Candidates> candidates = null;
-		
-		
-		if (dao.getConnection()){
-			comparison =dao.readAllComparison();
-			candidates=dao.readAllCandidates();
-		}
-		else {
+
+		if (dao.getConnection()) {
+			comparison = dao.readAllComparison();
+			candidates = dao.readAllCandidates();
+		} else {
 			System.out.println("No connection to database");
 		}
 		request.setAttribute("comparison", comparison);
 		request.setAttribute("candidates", candidates);
-		
-		RequestDispatcher rd=request.getRequestDispatcher("/jsp/results.jsp");
+
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/results.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
