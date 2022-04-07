e6be93eb72c815c2a745a556ec24e81a0acbbabc
Sipuli69 <toni20113@student.hamk.fi>
2022-04-07 15:26:17 +0300

    Jotain

--- vaali/src/main/java/app/Results.java
+++ vaali/src/main/java/app/Results.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import DAO.Dao;
-import data.Candidates;
+//import data.Candidates;
 import data.Comparison;
 
 /**
